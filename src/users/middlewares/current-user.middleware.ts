8c155903b53ac5dc9add47d4aa0063f9a9214117
Harsh Desai <harshedsai18502@gmail.com>
2023-05-29 18:53:50 +0530

    second commit

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -1,30 +1,22 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction } from 'express';
 import { UsersService } from '../users.service';
-// import { User } from '../user.entity';
+import { User } from '../user.entity';
 
-// declare global {
-//   // eslint-disable-next-line @typescript-eslint/no-namespace
-//   namespace Express {
-//     interface Request {
-//       currentUser?: User;
-//     }
-//   }
-// }
+interface IRequest extends Request {
+  session?: any;
+  currentUser?: User;
+}
 
 @Injectable()
 export class CurrentUserMiddleWare implements NestMiddleware {
   constructor(private usersService: UsersService) {}
 
-  async use(req: Request, res: Response, next: NextFunction) {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
+  async use(req: IRequest, res: Response, next: NextFunction) {
     const { userId } = req.session || {};
 
     if (userId) {
       const user = await this.usersService.findOne(userId);
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
       req.currentUser = user;
     }
 
